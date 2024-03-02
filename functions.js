123965a15741fc50b6b518e203ae6fa166982577
kyuugraphy <echo.of.psycho@gmail.com>
2024-03-02 13:25:41 +0100

    added how to make functions

---
+++ functions.js
@@ -0,0 +1,13 @@
+// how to make functions
+
+//1)
+function deposit1() {
+    return 1
+}
+
+const x = deposit1() // saves the result to the constant
+
+//2)
+const deposit2 = () => {
+    return 1
+}
---
+++ project.js
@@ -0,0 +1,13 @@
+// 1. deposit money
+// 2. determine number of lines to bet
+// 3. collecta bet amount
+// 4. spin the slot machine
+// 5. check the winning
+// 6. give user their winnings
+// 7. play again (?)
+
+function deposit() {
+    return
+}
+
+const x = deposit()
