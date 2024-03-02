1ff0cd22c98740198b5df861ba486af70424575e
kyuugraphy <echo.of.psycho@gmail.com>
2024-03-02 13:48:12 +0100

    deposit money + get number of betting lines

--- project.js
+++ project.js
@@ -10,3 +10,33 @@
 // "we require input and make it a function"
 const prompt = require("prompt-sync")();
 
+const deposit = () => {
+    while (true) {
+        const depositAmount = prompt ("Enter a deposit amount: ");
+        //now we need to convert it to a number, bcs input is default in string
+        const numberDepositAmout = parseFloat(depositAmount);
+
+        if (isNaN(numberDepositAmout) || numberDepositAmout <= 0) {
+            console.log("Invalid deposit amount, try again.");
+        } else {
+            return numberDepositAmout;
+        }
+    }
+};
+
+const getNumberOfLines = () => {
+    while (true) {
+        const lines = prompt ("Enter a number of lines to bet on (1-3): ");
+        //now we need to convert it to a number, bcs input is default in string
+        const numberOfLines = parseFloat(lines);
+
+        if (isNaN(numberOfLines) || numberOfLines >= 4 || numberOfLines <=0) {
+            console.log("Invalid betting number of lines, try again.");
+        } else {
+            return numberOfLines;
+        }
+    }
+}
+
+const depositAmount = deposit();
+const betLines = getNumberOfLines();
